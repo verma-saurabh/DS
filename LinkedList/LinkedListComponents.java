4271c39c4ac629754910470e46795cff6b20a3b3
Saurabh <saurabh.verma.miet@gmail.com>
2019-11-26 23:09:54 +0530

    LinkedListComponents

--- LinkedList/LinkedListComponents.java
+++ LinkedList/LinkedListComponents.java
@@ -1,4 +1,25 @@
 package LinkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedListComponents {
+
+
+    public int numComponents(ListNode head, int[] G) {
+        int ans = 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int a : G) set.add(a);
+
+        ListNode cur = head;
+
+        while (cur != null) {
+            if (set.contains(cur.val) && (cur.next == null || set.contains(cur.next.val))) {
+                ans++;
+            }
+            cur = cur.next;
+        }
+        return ans;
+    }
 }
