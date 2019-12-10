f3fd9d9d8b6a5de8ebef9c21aaaa48c3da2404eb
Saurabh <saurabh.verma.miet@gmail.com>
2019-12-10 21:59:47 +0530

    Array questions

--- Array/NumMatchingSubseq.java
+++ Array/NumMatchingSubseq.java
@@ -1,4 +1,36 @@
 package Array;
 
 public class NumMatchingSubseq {
+
+    public static void main(String[] args) {
+        String S = "abcde";
+        String[] words = {"a", "bb", "acd", "ace"};
+
+        NumMatchingSubseq obj = new NumMatchingSubseq();
+        System.out.println(obj.numMatchingSubseq(S, words));
+    }
+
+    public int numMatchingSubseq(String S, String[] words) {
+        int answer = 0;
+        for (String word : words) {
+            int count = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < word.length() && j < S.length()) {
+                if (word.charAt(i) == S.charAt(j)) {
+                    count++;
+                    i++;
+                    j++;
+                } else {
+                    j++;
+                }
+
+            }
+            if (count == word.length()) {
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
--- LinkedList/SwapNodesInPairs.java
+++ LinkedList/SwapNodesInPairs.java
@@ -1,4 +1,19 @@
 package LinkedList;
 
 public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+
+            int k = temp.val;
+            temp.val = temp.next.val;
+            temp.next.val = k;
+            temp = temp.next.next;
+
+        }
+        return head;
+    }
 }
