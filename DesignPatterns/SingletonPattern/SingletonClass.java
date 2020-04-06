530e86738786fa0adc5c48f7aa758749d6fa4967
Saurabh <saurabh.verma.miet@gmail.com>
2020-04-06 21:42:05 +0530

    Singleton Pattern

--- DesignPatterns/SingletonPattern/SingletonClass.java
+++ DesignPatterns/SingletonPattern/SingletonClass.java
@@ -1,4 +1,25 @@
 package DesignPatterns.SingletonPattern;
 
 public class SingletonClass {
+    private static SingletonClass instance;
+    private static boolean isInitialized = false;
+
+    private SingletonClass() {
+
+    }
+
+    private void init() {
+
+    }
+
+    public static synchronized SingletonClass getInstance() {
+        if (isInitialized) {
+            return instance;
+        } else {
+            instance = new SingletonClass();
+            instance.init();
+            isInitialized = true;
+            return instance;
+        }
+    }
 }
--- DesignPatterns/SingletonPattern/SingletonConcurrencySimulator.java
+++ DesignPatterns/SingletonPattern/SingletonConcurrencySimulator.java
@@ -1,4 +1,17 @@
 package DesignPatterns.SingletonPattern;
 
 public class SingletonConcurrencySimulator {
+    public static void main(String[] args) {
+        SingletonClass obj = SingletonClass.getInstance();
+
+        Thread thread1 = new Thread(() -> {
+            SingletonClass s = SingletonClass.getInstance();
+        });
+        Thread thread2 = new Thread(() -> {
+            SingletonClass s = SingletonClass.getInstance();
+        });
+
+        thread1.start();
+        thread2.start();
+    }
 }
