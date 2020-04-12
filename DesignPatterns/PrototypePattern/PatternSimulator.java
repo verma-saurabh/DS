ac130bf5866b2b82b13c073a8fdc9bdd1b2c3b89
Saurabh <saurabh.verma.miet@gmail.com>
2020-04-12 21:58:12 +0530

    Prototype Pattern

--- DesignPatterns/PrototypePattern/PatternSimulator.java
+++ DesignPatterns/PrototypePattern/PatternSimulator.java
@@ -1,4 +1,13 @@
 package DesignPatterns.PrototypePattern;
 
 public class PatternSimulator {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Rabbit rabbit = new Rabbit();
+        rabbit.setAge(2);
+        Rabbit rabbitCopy = rabbit.clone();
+        System.out.println(rabbit.getAge());
+        System.out.println(rabbitCopy.getAge());
+        rabbitCopy.setAge(3);
+        System.out.println(rabbitCopy.getAge());
+    }
 }
--- DesignPatterns/PrototypePattern/Rabbit.java
+++ DesignPatterns/PrototypePattern/Rabbit.java
@@ -1,4 +1,39 @@
 package DesignPatterns.PrototypePattern;
 
-public class Rabbit {
+public class Rabbit implements Cloneable {
+    public enum Breed {
+        HIMALYAN, AFRICAN, DUTCH
+    }
+
+    private int age;
+    private Breed breed;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public Rabbit() {
+
+    }
+
+    @Override
+    protected Rabbit clone() throws CloneNotSupportedException {
+        try {
+            return (Rabbit) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
