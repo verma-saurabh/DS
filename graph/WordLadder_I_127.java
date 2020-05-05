e6a4b7c07841a0a1e5f96a351ff0b7452f7ccf26
Saurabh <saurabh.verma.miet@gmail.com>
2020-05-05 18:19:43 +0530

    127. Word Ladder

---
+++ graph/WordLadder_I_127.java
@@ -0,0 +1,66 @@
+package graph;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class WordLadder_I_127 {
+
+    public static void main(String[] args) {
+        WordLadder_I_127 obj = new WordLadder_I_127();
+
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = Arrays.asList(new String[]{"hot", "dot", "dog", "lot", "log", "cog"});
+        System.out.println(obj.ladderLength(beginWord, endWord, wordList));
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int L = beginWord.length();
+
+        Map<String, List<String>> allComboDict = new HashMap<>();
+
+        wordList.forEach(
+                word -> {
+                    for (int i = 0; i < L; i++) {
+                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+                        List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
+                        transformations.add(word);
+                        allComboDict.put(newWord, transformations);
+                    }
+                }
+        );
+
+
+        Queue<Pair<String, Integer>> Q = new LinkedList<>();
+        Q.add(new Pair<>(beginWord, 1));
+
+        Map<String, Boolean> visited = new HashMap<>();
+
+        visited.put(beginWord, true);
+
+
+        while (!Q.isEmpty()) {
+            Pair<String, Integer> node = Q.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+
+            for (int i = 0; i < L; i++) {
+                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, L);
+
+                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
+                    if (adjacentWord.equals(endWord)) {
+                        return level + 1;
+                    }
+                    if (!visited.containsKey(adjacentWord)) {
+                        visited.put(adjacentWord, true);
+                        Q.add(new Pair<>(adjacentWord, level + 1));
+                    }
+                }
+            }
+
+        }
+
+        return 0;
+    }
+}
