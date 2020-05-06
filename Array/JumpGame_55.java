a39d70193cd47b74b51f4f8ef2fba51c8417bbb1
Saurabh <saurabh.verma.miet@gmail.com>
2020-05-06 11:20:52 +0530

    55 Jump Game I leetcode 55

---
+++ Array/JumpGame_55.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class JumpGame_55 {
+    public static void main(String[] args) {
+        JumpGame_55 obj = new JumpGame_55();
+        int[] nums = {3, 2, 1, 0, 4};
+        System.out.println(obj.canJump(nums));
+    }
+
+    public boolean canJump(int[] A) {
+        if (A.length <= 1)
+            return true;
+
+        int max = A[0]; //max stands for the largest index that can be reached.
+
+        for (int i = 0; i < A.length; i++) {
+            //if not enough to go to next
+            if (max <= i && A[i] == 0)
+                return false;
+
+            //update max
+            if (i + A[i] > max) {
+                max = i + A[i];
+            }
+
+            //max is enough to reach the end
+            if (max >= A.length - 1)
+                return true;
+        }
+
+        return false;
+    }
+}
