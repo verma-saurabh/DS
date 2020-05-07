3e636e7008e285a1813a7810ae642ce7fb6ad107
Saurabh <saurabh.verma.miet@gmail.com>
2020-05-07 18:29:44 +0530

    1365. How Many Numbers Are Smaller Than the Current Number

--- Array/SmallerThanCurrent.java
+++ Array/SmallerThanCurrent.java
@@ -1,4 +1,24 @@
 package Array;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+/*
+1365. How Many Numbers Are Smaller Than the Current Number
+ */
 public class SmallerThanCurrent {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        List<Integer> list = IntStream.of(nums).boxed().collect(Collectors.toList());
+
+        Collections.sort(list);
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = list.indexOf(nums[i]);
+        }
+
+        return ans;
+    }
 }
