55068358fcf6dbce30ea87a3998def4e02c1d137
Saurabh <saurabh.verma.miet@gmail.com>
2020-05-21 21:08:44 +0530

    Merge Sort

--- SortingAlgorithms/MergeSort.java
+++ SortingAlgorithms/MergeSort.java
@@ -1,4 +1,63 @@
 package SortingAlgorithms;
 
+
 public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        MergeSort obj = new MergeSort();
+        obj.mergeSort(arr, 0, arr.length -1);
+
+        for (int i : arr) {
+            System.out.print(i + " ,");
+        }
+
+    }
+
+    public void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+
+        }
+    }
+
+    public void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+
+        for (int i = 0; i < n2; ++i)
+            R[i] = arr[m + i + 1];
+
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 }
