cb4e24ae46b0807c7d6d034b6595efae2fd354a7
Saurabh <saurabh.verma.miet@gmail.com>
2020-05-25 20:01:02 +0530

    3. Longest Substring Without Repeating Characters

---
+++ String/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,31 @@
+package String;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+3. Longest Substring Without Repeating Characters
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String str = "abccab";
+        LongestSubstringWithoutRepeatingCharacters obj = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(obj.lengthOfLongestSubstring(str));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
