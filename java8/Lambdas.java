6b517f17ce9a6611e4fc9db1ab47d4e0af276a49
Saurabh <saurabh.verma.miet@gmail.com>
2020-06-09 22:38:36 +0530

    Java 8 Lambdas

--- java8/Lambdas.java
+++ java8/Lambdas.java
@@ -1,4 +1,23 @@
 package java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Lambdas {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(new String[]{"hello", "hola", "hallo", "ciao"});
+        Stream<String> stream1 = Stream.of("hello", "hola", "hallo", "ciao").sorted();
+        Stream<String> stream = list.stream();
+        System.out.println(stream);
+        stream.forEach((s) -> {
+            System.out.println(s);
+        });
+
+        stream1.forEach((s) -> {
+            System.out.println(s);
+        });
+
+    }
 }
