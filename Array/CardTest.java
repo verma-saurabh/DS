55fb348d40b74f6dc2cb312b60a7e23b220c4c96
Saurabh <saurabh.verma.miet@gmail.com>
2020-06-11 22:46:33 +0530

    code for playing card comparison

--- Array/CardTest.java
+++ Array/CardTest.java
@@ -1,12 +1,15 @@
 package Array;
 
+import java.util.Comparator;
+
 enum Rank {
-    one(1), two(2),three(3), four(4), five(5), six(6), seven(7), eight(8), nine(9),
+    one(1), two(2), three(3), four(4), five(5), six(6), seven(7), eight(8), nine(9),
     ten(10), jack(11), queen(12), king(13), ace(14);
 
     private int value;
-    Rank(int value){
-        this.value=value;
+
+    Rank(int value) {
+        this.value = value;
     }
 
 }
@@ -21,11 +24,35 @@ enum Suit {
 
 }
 
- class Card {
+class Card implements Comparable<Card> {
     Suit suit;
     Rank rank;
+
+    Card(Rank rank) {
+        this.rank = rank;
+    }
+
+    Card of(Suit suit) {
+        this.suit = suit;
+        return this;
+    }
+
+    @Override
+    public int compareTo(Card card) {
+        if(this.suit==card.suit){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
 }
 
-public class CardTest{
+public class CardTest {
+    public static void main(String[] args) {
+        Card card1 = new Card(Rank.six).of(Suit.Culb);
+        Card card2 = new Card(Rank.six).of(Suit.Diamond);
 
+        card1.compareTo(card2);
+
+    }
 }
