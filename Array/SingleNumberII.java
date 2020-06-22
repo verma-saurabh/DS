23732ce289b8cd9912db6f4695006b94a717af3d
Saurabh <saurabh.verma.miet@gmail.com>
2020-06-22 16:38:19 +0530

    137. Single Number II

---
+++ Array/SingleNumberII.java
@@ -0,0 +1,18 @@
+package Array;
+
+/*
+137. Single Number II
+ */
+public class SingleNumberII {
+    public int singleNumber(int[] A) {
+        int ones = 0, twos = 0, threes = 0;
+        for (int i = 0; i < A.length; i++) {
+            twos |= ones & A[i];
+            ones ^= A[i];
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+}
