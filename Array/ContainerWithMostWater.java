133e5c0743a587e80d39a5e889526b2dbb262ee2
Saurabh2 Verma <saurabh.verma.miet@gmail.com>
2021-01-06 20:02:35 +0530

    ContainerWithMostWater

--- Array/ContainerWithMostWater.java
+++ Array/ContainerWithMostWater.java
@@ -1,2 +1,40 @@
-package Array;public class ContainerWithMostWater {
+package Array;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+        System.out.println(obj.maxArea(height));
+        System.out.println(obj.maxArea_TwoPointer(height));
+    }
+
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            int area = 0;
+            for (int j = i + 1; j < height.length; j++) {
+                area = (j - i) * Math.min(height[i], height[j]);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+
+    public int maxArea_TwoPointer(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int maxArea = 0;
+
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+
+            if (height[l] < height[r]) {
+                l++;
+            } else
+                r--;
+
+        }
+        return maxArea;
+    }
 }
